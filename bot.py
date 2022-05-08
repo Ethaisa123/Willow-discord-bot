8696c27448533913bea35dd71444f8a8a02b9143
Ethan van Wijk <100451791+Ethaisa123@users.noreply.github.com>
2022-05-08 13:21:57 +1200

    changed jesus command and added better comments

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 
-#installs all nessisary pips removed so that code can run on rasberry pi
+#installs all nessisary pips (removed so that code can run on rasberry pi)
 """
 try:
     import lightbulb
@@ -16,17 +16,18 @@ import hikari
 import datetime
 from dotenv import load_dotenv
 import requests 
+from random import seed
+from random import randint
 
-
+#token stripping (please add your own token below!)
 TOKEN = "OTY5MzI3NzUxNzc1NTMxMDQ4.  Ymry0Q.rVeFlbGXxc10HQrPQrjLJ_Xa8Hk"
 print(TOKEN)
 TOKEN = TOKEN.replace(" ", "")
-#generating files
+seed(1)
 
 
 #bad bad bad enifficent code
 #I GIVE UP IF IT WORKS IT WORKS
-
 def verse_funct_text(r):
     
 
@@ -52,7 +53,6 @@ def verse_funct_text(r):
     print(final_verse_text)
     return(final_verse_text)
 
-
 def verse_funct_image(r):
 
     #getting the image of the verse
@@ -76,7 +76,7 @@ diff = future - today
 days_until = str(diff.days)
 
 
-#authing discord bot
+#authing discord bot and adding servers
 bot = lightbulb.BotApp(
     token=TOKEN,
     default_enabled_guilds=(970284254925840414, 733440457618620417, 952065165988339722, 970282983976230963)
@@ -89,19 +89,29 @@ async def on_start(event):
     print("dish bot Esh workin")
 
 
-#sayying the ammount of days when called
+#DAYS UNITL CAMP COMMAND
 @bot.command
 @lightbulb.command("time-until-camp", "tells you the ammount of days until the next camp")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx):
     await ctx.respond("It is {} days until camp!" .format(days_until))
 
+#WEBSITE COMMAND
 @bot.command
 @lightbulb.command("website", "the bot github website!")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx):
-    await ctx.respond("https://github.com/Ethaisa123/Willow-discord-bot")
+    value = randint(0, 3)
+    jesus = [
+    "https://github.com/Ethaisa123/Willow-discord-bot",
+    "https://static.wikia.nocookie.net/headhuntersholosuite/images/d/d8/Jesus_Christ.jpg/revision/latest/scale-to-width-down/225?cb=20171123000925",
+    "https://wp.en.aleteia.org/wp-content/uploads/sites/2/2018/10/web3-christ-pantocrator-pd.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg/1200px-The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg"
+    ]
+    await ctx.respond(jesus[value])
 
+
+#JESUS COMMAnD
 @bot.command
 @lightbulb.command("jesus", "jesus!")
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -151,7 +161,7 @@ async def add(ctx):
     f.write("{} : {} : {} \n" .format(ctx.author.username, ctx.options.bug ,str(date)) )
     f.close()
 
-
+#VIDEOS COMMAND
 @bot.command()
 @lightbulb.option("camps", "the videos from different camps", choices=["retro-camp", "futuristic-camp", "olympic-camp", "aquatic-camp"], required=True)
 @lightbulb.command("camp-videos", "the videos from different camps")
@@ -166,9 +176,9 @@ async def echo(ctx):
     elif ctx.options.camps == "aquatic-camp":
         await ctx.respond("aquatic camp video: https://youtu.be/StextHDOZw8/")
 
-
+#PHOTOS COMMAND
 @bot.command()
-@lightbulb.option("camps", "the photos from different camps", choices=["retro-camp", "futuristic-camp-N/A", "olympic-camp-N/A", "aquatic-camp-N/A"], required=True)
+@lightbulb.option("camps", "the photos from different camps (contact Ethan to add photos)", choices=["retro-camp", "futuristic-camp-N/A", "olympic-camp-N/A", "aquatic-camp-N/A"], required=True)
 @lightbulb.command("camp-photos", "the photos from different camps")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def echo(ctx):
@@ -182,10 +192,5 @@ async def echo(ctx):
         await ctx.respond("aquatic camp photos: https://photos.app.goo.gl/iBAq9KHLn3SfvLmC6")
 
 
-
-"""
-https://photos.app.goo.gl/Y6AJQGjoeTibqiib7
-
-"""
 
 bot.run()
